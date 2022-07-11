17a40f1aa2eac4aee96070a3eebd00e101164d6d
kreema1602 <100921862+kreema1602@users.noreply.github.com>
2022-07-11 08:03:06 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,48 +1,51 @@
 import socket
-
+import json
 HOST = '127.0.0.1'
-PORT = 8001
+PORT = 8000
 
 
-def login(listacc):
+def login(dictacc):
     username = client.recv(1024).decode()
     password = client.recv(1024).decode()
-    for i in range(len(listacc[0])):
-        if username == listacc[0][i]:
-            if password == listacc[1][i]:
-                client.send('Login successfully'.encode())
-                return True
+    for i in dictacc:
+        if username == i['username'] and password == i['password']:
+            client.send('Login successfully'.encode())
+            return True
     client.send('Login fail'.encode())
     return False
 
 
-def signup(listacc):
+def signup(dictacc):
     username = client.recv(1024).decode()
     password = client.recv(1024).decode()
     bankno = client.recv(1024).decode()
-    for i in range(len(listacc)):
-        if username == listacc[0][i]:
+    for i in dictacc:
+        if username == i['username']:
             client.send('Sign up fail'.encode())
             return False
-    listacc[0].append(username)
-    listacc[1].append(password)
-    listacc[2].append(bankno)
+    newuser = {'username': username, 'password': password, 'bank account number': bankno}
+    dictacc.append(newuser)
+    f = open('account.json', 'w')
+    json_object = json.dumps(dictacc, indent=4)
+    f.write(json_object)
     client.send('Sign up successfully'.encode())
     return True
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
-sock.listen(1)
+sock.listen(2)
 print('Waiting for connection......')
 client, addr = sock.accept()
 
-listt = [['luan', 'tin', 'phuc'], ['123', '123', '123'], ['0123456789', '0123456789', '0123456789']]
+fileacc = open('account.json')
+dataacc = json.load(fileacc)
 print('Connected by', addr)
+
 choose = client.recv(1024).decode()
 if choose == '1':
-    login(listt)
+    login(dataacc)
 if choose == '2':
-    signup(listt)
+    signup(dataacc)
 
 sock.close()
