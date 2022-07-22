88add0bd51047f423a82f9263ed7c1871c204d82
kreema1602 <100921862+kreema1602@users.noreply.github.com>
2022-07-22 18:54:18 +0700

    final

--- client.py
+++ client.py
@@ -1,22 +1,151 @@
+import json
+from tkinter import *
+from PIL import ImageTk, Image
 import socket
+import datetime
 
 HOST = '127.0.0.1'
-PORT = 8000
+PORT = 8003
 
 
+def recvimage(hotelname, dicthotel):
+    root = Tk()
+    imagelist =[]
+    for i in range(len(dicthotel)):
+        f = open('client/'+dicthotel[hotelname][i]['no']+'.jpg', 'wb')
+        data = sock.recv(100000000)
+        f.write(data)
+        imagelist.append(ImageTk.PhotoImage(Image.open('client/'+dicthotel[hotelname][i]['no']+'.jpg')))
+        Label(image=imagelist[i]).grid(row=0, column=i)
+        Label(text=dicthotel[hotelname][i]['no']).grid(row=1, column=i)
+        sock.send(bytes(1024))
+    root.mainloop()
+
+    
+def booking_menu(list_of_no):
+    while True:
+        booking_choices = ['0. Search for other hotels', '1. Booking']
+        for i in booking_choices:
+            print(i)
+        b_choices = input('Your choice: ')
+        sock.send(b_choices.encode())
+        if b_choices == '1':
+            print('Enter 0 to stop')
+            list_of_booking = []
+            booking_choice = input('No. of rooms: ')
+            if booking_choice != '0' and booking_choice in list_of_no:
+                list_of_booking.append(booking_choice)
+                while booking_choice != '0' and booking_choice in list_of_no:
+                    booking_choice = input('No. of rooms: ')
+                    list_of_booking.append(booking_choice)
+            sock.send(str(list_of_booking).encode())
+            ans = eval(sock.recv(1024).decode())
+            print('Booking successfully')
+            print('Total cost:', ans)
+            datetime_data = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+            sock.send(datetime_data.encode())
+            break
+        else:
+            break
+        
+def menu(username):
+    while True:
+        menu_choices = ['0. Exit', '1. Search for room', '2. List of order made', '3. List of hotels']
+        for i in menu_choices:
+            print(i)
+        m_choices = input('Your choice: ')
+        sock.send(m_choices.encode())
+        if m_choices == '1':
+            ans = input('Hotel name: ')
+            sock.send(ans.encode())
+            # luan
+            strin = sock.recv(1024)
+            hotel_dict = json.loads(strin)
+            recvimage(ans, hotel_dict)
+
+
+            rep = sock.recv(1024).decode()
+            if rep == 'Fail':
+                print('Not found')
+            else:
+                print('Check in time: ')
+                while True:
+                    t_year = int(input('Year: '))
+                    t_month = int(input('Month: '))
+                    t_date = int(input('Date: '))
+                    t_time_checkin = datetime.date(t_year, t_month, t_date)
+                    if t_time_checkin < datetime.date.today():
+                        print('Check in date invalid')
+                    else:
+                        break
+                print('Check out time: ') 
+                while True:
+                    t_year = int(input('Year: '))
+                    t_month = int(input('Month: '))
+                    t_date = int(input('Date: '))
+                    t_time_checkout = datetime.date(t_year, t_month, t_date)
+                    if t_time_checkin > t_time_checkout:
+                        print('Check out time invalid')
+                    else:
+                        break
+                info = (ans, t_time_checkin, t_time_checkout)
+                data_info = str(info)
+                sock.send(data_info.encode())
+                list_of_room = sock.recv(1024).decode()
+                list_hot = eval(list_of_room)
+                if len(list_hot) > 0:
+                    list_of_no = []
+                    for i in list_hot:
+                        print('No.:' , i['no'])
+                        print('Number of bed:', i['kind'])
+                        print('Description:', i['des'])
+                        print('Price:', i['price'], 'dollar a night')
+                        list_of_no.append(i['no'])
+                    print("-"*20)
+                    booking_menu(list_of_no)
+                else:
+                    print('No room available')
+        elif m_choices == '2':
+            ans = input('Hotel name: ')
+            sock.send(ans.encode())
+            list_of_order = eval(sock.recv(1024).decode())
+            if len(list_of_order) == 0:
+                print('You have not made any order at this hotel')
+            else:
+                for i in range(len(list_of_order)):
+                    print(i + 1, list_of_order[i])
+                code = input('Choose order to delete: ')
+                code = list_of_order[int(code) - 1]
+                sock.send(code.encode())
+                ans = sock.recv(1024).decode()
+                if ans == 'OK':
+                    ans = sock.recv(1024).decode()
+                    if ans == 'OK':
+                        print('Cancel successfully')
+                    else:
+                        print('It is over 24 hours, you cannot delete your order')
+                elif ans == 'Not found':
+                    print('Not found')
+        elif m_choices == '3':
+            list_of_hot = sock.recv(1024).decode()
+            list_of_hot = eval(list_of_hot)
+            print(list(list_of_hot))
+        else: 
+            break
+    
+        
 def clogin():
     ans = input('Username: ')
+    username = ans
     sock.send(ans.encode())
     ans = input('Password: ')
     sock.send(ans.encode())
-    checklogin = sock.recv(1024).decode()
-    if checklogin == '1':
-        print('Login successful')
-        return True
-    if checklogin == '0':
+    ans = sock.recv(1024).decode()
+    if ans == 'OK':
+        print('Login success')
+        menu(username)
+    else:
         print('Login fail')
-        return False
-
 
 def csignup():
     ans = input('Username: ')
@@ -25,31 +154,30 @@ def csignup():
     sock.send(ans.encode())
     ans = input('Bank account no: ')
     sock.send(ans.encode())
-    checksignup = sock.recv(1024).decode()
-    if checksignup == '1':
-        print('Sign up successful')
-        return True
-    if checksignup == '0':
+    ans = sock.recv(1024).decode()
+    if ans == 'OK':
+        print('Sign up success')
+        start_menu()
+    else:
         print('Sign up fail')
-        return False
 
+def start_menu():
+    choices = ['0. Exit', '1. Login', '2. Sign up']
+    for i in choices:
+        print(i)
+    choose = input('Your choice: ')
 
- def menu():
-    print('1.Login\t 2.Sign up')
-    choose = input()
     if choose == '1':
         sock.send('1'.encode())
-        while not clogin():
-            clogin()
-    if choose == '2':
+        clogin()
+    elif choose == '2':
         sock.send('2'.encode())
-        while not csignup():
-            csignup()
-
-            
+        csignup()
+    
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('You are connecting to', HOST, PORT)
 sock.connect((HOST, PORT))
-menu()
+
+start_menu()
 
 sock.close()
--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 import socket
 import json
-import threading
+import datetime
+
 HOST = '127.0.0.1'
-PORT = 8000
+PORT = 8003
+
 LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 NUMBERS = '0123456789'
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST, PORT))
+sock.listen(2)
+print('Waiting for connection......')
+client, addr = sock.accept()
+
+
+def sendimage(hotelname, dicthotel):
+    for i in range(len(dicthotel)):
+        f = open('data/'+hotelname+'/'+dicthotel[hotelname][i]['no']+'.jpg','rb')
+        client.send(f.read())
+        client.recv(1024)
 
 
 def checkvalidacc(username, password, bankno):
@@ -28,10 +43,10 @@ def login(account_list):
     password = client.recv(1024).decode()
     for i in account_list:
         if username == i['username'] and password == i['password']:
-            client.send('1'.encode())
-            return True
-    client.send('0'.encode())
-    return False
+            client.send('OK'.encode())
+            return (True, username)
+    client.send('Fail'.encode())
+    return (False, '')
 
 
 def signup(account_list):
@@ -39,50 +54,170 @@ def signup(account_list):
     password = client.recv(1024).decode()
     bankno = client.recv(1024).decode()
     if not checkvalidacc(username, password, bankno):
-        client.send('0'.encode())
+        client.send('Fail'.encode())
         return False
     for i in account_list:
         if username == i['username']:
-            client.send('0'.encode())
+            client.send('Fail'.encode())
             return False
     newuser = {'username': username, 'password': password, 'bank account number': bankno}
     account_list.append(newuser)
     f = open('data/account.json', 'w')
     json_object = json.dumps(account_list, indent=4)
     f.write(json_object)
-    client.send('1'.encode())
     f.close()
+    client.send('OK'.encode())
     return True
 
-def handleClient(client, addr):
-    file_of_account = open('account.json')
-    raw_account_list = json.load(file_of_account)
-    print('Connected by', addr)
+def search_for_room(dict_of_hotel, ans):
+    print(ans[0])
+    list_of_available_room = []
+    for i in dict_of_hotel[ans[0]]:
+        checkn = 0
+        for j in i['booked']:
+            checkout = datetime.datetime.strptime(j['checkout'], '%Y-%m-%d')
+            if checkout.date() > ans[1]:
+                checkn = checkn + 1
+        if checkn == 0:
+            list_of_available_room.append(i)
+    return list_of_available_room
+    
+def booking_menu(list_of_valid_room, ans, username):
+    while True:
+        sub_ans = client.recv(1024).decode()
+        if sub_ans == '1':
+            list_of_booked = client.recv(1024).decode()
+            list_of_booked = eval(list_of_booked)
+            cost = 0
+            for i in list_of_valid_room:
+                if i['no'] in list_of_booked:
+                    cost = cost + i['price'] * ((ans[2] - ans[1]).days)
+            client.send(str(cost).encode())
+            order_data_str = client.recv(1024).decode()
+            order_time = datetime.datetime.strptime(order_data_str, '%Y-%m-%d %H:%M:%S')
+            code = str(order_time.year) + str(order_time.month) + str(order_time.day)
+            code = code + str(order_time.hour) + str(order_time.minute) + str(order_time.second)
+            f = open('data/order.json')
+            order_dict = json.load(f)
+            f.close()
+            f = open('data/hoteldata.json')
+            hotel_dict = json.load(f)
+            f.close()
+            list_of_no = list_of_booked[:len(list_of_booked) - 1]
+            new_order = {
+                "hotel": ans[0], 
+                "order_time": order_data_str,
+                "no": list_of_no, 
+                "checkin": datetime.datetime.strftime(ans[1], '%Y-%m-%d'),
+                "checkout": datetime.datetime.strftime(ans[2], '%Y-%m-%d')
+            }
+            order_dict[username][code] = new_order
+            for i in hotel_dict[ans[0]]:
+                if i['no'] in list_of_booked:
+                    new_hot_order = {
+                        "booker": username, 
+                        "order_time": order_data_str,
+                        "checkin": datetime.datetime.strftime(ans[1], '%Y-%m-%d'),
+                        "checkout": datetime.datetime.strftime(ans[2], '%Y-%m-%d')
+                    }
+                    i['booked'].append(new_hot_order)
+            if len(list_of_no) > 0:
+                with open('data/order.json', 'w') as f:
+                    json.dump(order_dict, f, indent=4)
+                with open('data/hoteldata.json', 'w') as f:
+                    json.dump(hotel_dict, f, indent = 4)
+            break
+        else:
+            break
+
+
+def menu_listener(username):
+    while True:
+        ans = client.recv(1024).decode()
+        if ans == '1':
+            hotel_name = client.recv(1024).decode()
+            f = open('data/hoteldata.json')
+            hotel_dict = json.load(f)
+            f.close()
+
+            # luan
+            data = json.dumps(hotel_dict)
+            client.send(data.encode())
+            sendimage(hotel_name, hotel_dict)
+
+
+            if hotel_name not in hotel_dict:
+                client.send('Fail'.encode())
+            else:
+                client.send('OK'.encode())
+                ans = client.recv(1024).decode()
+                ans = eval(ans)
+                list_of_available_room = search_for_room(hotel_dict, ans)
+                client.send(str(list_of_available_room).encode())
+                if len(list_of_available_room) > 0:
+                    booking_menu(hotel_dict[ans[0]], ans, username)
+        elif ans == '2':
+            hotel_name = client.recv(1024).decode()
+            f = open('data/order.json')
+            order_dict = json.load(f)
+            f.close()
+            list_of_order = []
+            for k, v in order_dict[username].items():
+                if v['hotel'] == hotel_name:
+                    list_of_order.append(k)
+            client.send(str(list_of_order).encode())
+            if len(list_of_order) > 0:
+                code = client.recv(1024).decode()
+                if code not in order_dict[username]:
+                    client.send('Not found'.encode())
+                else:
+                    client.send('OK'.encode())
+                    key = order_dict[username][code]['order_time']
+                    key_time = datetime.datetime.strptime(key, '%Y-%m-%d %H:%M:%S')
+                    if (key_time - datetime.datetime.now()).total_seconds() < 24.0 * 3600:
+                        client.send('OK'.encode())
+                        order_dict[username].pop(code)
+                        with open('data/order.json', 'w') as f:
+                            json.dump(order_dict, f, indent = 4)
+                        #delete in hoteldatajson
+                        f = open('data/hoteldata.json')
+                        hotel_dict = json.load(f)
+                        f.close()
+                        for i in hotel_dict[hotel_name]:
+                            tmp_list = []
+                            for j in i['booked']:
+                                if j['order_time'] != key:
+                                    tmp_list.append(j)
+                            i['booked'] = tmp_list
+                        with open('data/hoteldata.json', 'w') as f:
+                            json.dump(hotel_dict, f, indent = 4)
+                    else:
+                        client.send('Fail'.encode())    
+        elif ans == '3':
+            f = open('data/hoteldata.json')
+            hotel_dict = json.load(f)
+            f.close()
+            keys = list(hotel_dict.keys())
+            client.send(str(keys).encode())
+        else:
+            break
+        
+def init_listener():
     choose = client.recv(1024).decode()
     if choose == '1':
-        while not login(raw_account_list):
-            login(raw_account_list)
-    if choose == '2':
-        while not signup(raw_account_list):
-            signup(raw_account_list)
-    print("Client", addr, "finished")
-    file_of_account.close()
-    client.close()
+        username = ''
+        check, username = login(raw_account_list)
+        if check == True:
+            menu_listener(username)
+    elif choose == '2':
+        signup(raw_account_list)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
-sock.listen(2)
-print('Waiting for connection......')
 
-nClient = 0
-#allow up to 50 clients access
-while(nClient < 50):
-    client, addr = sock.accept()
 
-    thr = threading.Thread(target=handleClient, args=(client,addr))
-    thr.daemon = False
-    thr.start()
-
-    nClient += 1
-
+file_of_account = open('data/account.json')
+raw_account_list = json.load(file_of_account)
+print('Connected by', addr)
+init_listener()
+file_of_account.close()
+    
 sock.close()
